42e0bd732bcd3a9f6a54d890a194d75bbcd3d8fe
kuzminos <kuzminos22@gmail.com>
2017-04-16 11:45:56 +0300

    Playing with WebPack

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by kuzmi on 4/16/2017.
+ */
+import './index.css'
+import numeral from 'numeral';
+
+const value = numeral(1000).format('$0,0.00');
+console.log(`${value} problems`);
